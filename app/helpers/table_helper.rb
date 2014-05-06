9faf0ea6a76423410dda86d433cfe4f5b0b82ad6
Ryan Bigg <radarlistener@gmail.com>
2014-05-06 19:37:34 +1000

    Show details of people

--- app/helpers/table_helper.rb
+++ app/helpers/table_helper.rb
@@ -1,2 +1,17 @@
 module TableHelper
+  def person_details(person)
+    details = ["*"]
+    case person.dietary_requirement
+      when "Special"
+        details << "S"
+        details << person.other_requirements
+      when "Dairy Free"
+        details << "D"
+      when "Gluten Free"
+        details << "GF"
+      when "Allergy"
+        details << "Allergy #{person.other_requirements}"
+    end
+    "[#{details.join(', ')}]"
+  end
 end
